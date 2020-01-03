d71663b3b1612971aed4fc214f794920509ef9ee
Nick <nicknewburg@gmail.com>
2020-01-03 14:10:33 -0800

    began working on projects view shell, and adding projects to main repo

--- server.js
+++ server.js
@@ -12,14 +12,26 @@ const PORT = 8080;
 const path = require('path');
 
 app.set("view engine", "ejs");
+app.set('views', [path.join(__dirname, 'views'),
+                      path.join(__dirname, 'views/barChart/')]);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('views');
+
 
 
 app.get("/", (req, res) => {
      return res.render("index");
   })
 
+app.get("/projects", (req, res) => {
+     return res.render("projects");
+  })
+
+app.get("/barChart", (req, res) => {
+     return res.render("/barChart/barChartIndex");
+  })
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
