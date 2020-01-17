f009fc8e7fb35297cf60cf5857847fad8e414a8f
Nick <nicknewburg@gmail.com>
2020-01-16 17:01:42 -0800

    adjusted parts of biography section

--- public/scripts/swapBio.js
+++ public/scripts/swapBio.js
@@ -3,10 +3,13 @@
   $("#codingApt").hide();
   $("#personalityTra").hide();
 
+  $("#bioText").text("My History");
+
   $( "#switchHist" ).click(function() {
     $("#codingApt").hide();
     $("#personalityTra").hide();
     $("#history").show();
+    $("#bioText").text("My History");
 
   })
 
@@ -14,12 +17,15 @@
     $("#personalityTra").hide();
     $("#history").hide();
     $("#codingApt").show();
+    $("#bioText").text("Coding Aptitude");
   })
 
   $( "#switchPersonality" ).click(function() {
     $("#codingApt").hide();
     $("#history").hide();
     $("#personalityTra").show();
+
+    $("#bioText").text("Personal Traits");
 
   })
 
---
+++ public/scripts/textTrans.js
@@ -0,0 +1,25 @@
+  $(function( $ ){
+
+
+
+    for(let n = 1; n <= 4; n++){
+
+        var text = document.getElementById('text' + n);
+
+        var newDom = '';
+        var animationDelay = 4;
+
+        for(let i = 0; i < text.innerText.length; i++)
+        {
+            newDom += '<span class="char">' + (text.innerText[i] == ' ' ? '&nbsp;' : text.innerText[i])+ '</span>';
+        }
+
+        text.innerHTML = newDom;
+        var length = text.children.length;
+
+        for(let i = 0; i < length; i++)
+        {
+            text.children[i].style['animation-delay'] = animationDelay * i + 'ms';
+        }
+   }
+});
