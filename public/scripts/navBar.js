3ff9fc96f871536aabc9a9b561f0d73f14cc5ffb
Nick <nicknewburg@gmail.com>
2020-08-10 16:53:43 -0700

    biography

--- public/scripts/navBar.js
+++ public/scripts/navBar.js
@@ -22,7 +22,7 @@
       $(nameTitle).append(header)
       $(navBar).append(nameTitle)
 
-     $(navBar).append(createCategory('/aboutme', 'About Me'))
+     $(navBar).append(createCategory('/aboutme', 'Biography'))
      $(navBar).append(createCategory('/projects', 'Projects'))
      $(navBar).append(createCategory('/resume', 'Resume'))
 
@@ -33,7 +33,7 @@
         let navBar = $("<div></div>");
       $(navBar).attr( 'id', 'nav' );
 
-        let hamburger = '<nav class="navbar navbar-light navbar-1 white"><a class="navbar-brand" href="/">NICK NEWBURG</a><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent15"aria-controls="navbarSupportedContent15" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button><div class="collapse navbar-collapse" id="navbarSupportedContent15"><ul class="navbar-nav mr-auto"><li class="nav-item active"><a class="nav-link" href="/aboutme">About Me <span class="sr-only">(current)</span></a></li><li class="nav-item"><a class="nav-link" href="/projects">Projects</a></li><li class="nav-item"><a class="nav-link" href="/resume">Resume</a></li></ul></div></nav>';
+        let hamburger = '<nav class="navbar navbar-light navbar-1 white"><a class="navbar-brand" href="/">NICK NEWBURG</a><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent15"aria-controls="navbarSupportedContent15" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button><div class="collapse navbar-collapse" id="navbarSupportedContent15"><ul class="navbar-nav mr-auto"><li class="nav-item active"><a class="nav-link" href="/aboutme">Biography <span class="sr-only">(current)</span></a></li><li class="nav-item"><a class="nav-link" href="/projects">Projects</a></li><li class="nav-item"><a class="nav-link" href="/resume">Resume</a></li></ul></div></nav>';
 
       $('#newNav').append($.parseHTML(hamburger))
 
